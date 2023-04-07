abe2ed0e11736061afda00f8804b9890ef65d01a
seokscribe <118441395+registaPark@users.noreply.github.com>
2023-04-07 21:31:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,23 +1,6 @@
-import java.util.Arrays;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] arr = new String[n];
-        
-        for(int i=0;i<arr1.length;i++){
-            char[] a = new char[arr1.length];
-            int k = 1;
-            Arrays.fill(a,' ');
-            int c = arr1[i];
-            int d = arr2[i];
-            while(k<=n){
-                if(c%2==1 || d%2==1) a[n-k]='#';
-                k++;
-                c=c/2;
-                d=d/2;
-            }
-            arr[i] = new String(a);
-        }
-        
-        return arr;
+        String[] answer = {};
+        return answer;
     }
 }
