119712d221b5d567747e5a9b4272dd53a359b4cd
seokscribe <118441395+registaPark@users.noreply.github.com>
2023-08-19 18:32:09 +0900

    [Gold I] Title: 외판원 순회, Time: 272 ms, Memory: 23244 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int[][] map;
+	static int[][] dp;
+	static final int INF = 11000000;
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.valueOf(br.readLine());
+		map = new int[n][n];
+		dp = new int[n][(1 << n) - 1];
+		
+		StringTokenizer st;
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				int weight = Integer.valueOf(st.nextToken());
+				map[i][j] = weight;
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		
+		System.out.println(dfs(0, 1));
+	}
+	
+	private static int dfs(int city, int visitBitmask) {
+		
+		if(visitBitmask == (1 << n) - 1) {	
+			if(map[city][0] == 0) {	
+				return INF;
+			}
+			
+			return map[city][0];	
+		}
+		
+		if(dp[city][visitBitmask] != -1) {	
+			return dp[city][visitBitmask];
+		}
+		
+		for(int i = 0; i < n; i++) {	
+			if((visitBitmask & (1 << i)) == 0 && map[city][i] != 0) {	
+                if(dp[city][visitBitmask]==-1) dp[city][visitBitmask] = dfs(i, visitBitmask | (1 << i)) + map[city][i];
+				dp[city][visitBitmask] = Math.min(dp[city][visitBitmask], dfs(i, visitBitmask | (1 << i)) + map[city][i]);
+			}
+		}
+		if(dp[city][visitBitmask]==-1){
+            return INF;
+        }
+		return dp[city][visitBitmask];
+	}
+}
